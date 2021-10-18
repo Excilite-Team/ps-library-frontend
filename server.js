b933d21d87c42fb5be15e8a0e754d7bf8403004e
mcpeblocker <mcpeblocker@gmail.com>
2021-10-18 19:53:24 +0500

    production changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req,res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = require('./src/config.json').PORT || 5555;
+
+app.listen(PORT, () => {
+    console.log('App started on port ' + PORT);
+});
